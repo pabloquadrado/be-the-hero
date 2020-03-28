8874009e906553f4f12b9beca360ad4fdde42aab
Pablo Rodrigues Quadrado <pabloquadrado27@gmail.com>
2020-03-28 10:18:17 -0300

    Separando projeto em três repositórios: backend, mobile e frontend.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Be The Hero
 
-Aplicação construída do zero utilizando 100% da stack JavaScript com Node.js, React.js e React Native. A aplicação consiste em ser um sistema para ONGs pequenas, onde na parte web as ONGs cadastram o caso e o valor e na parte mobile há o contato da ONG para quem quiser contribuir.
+Aplicação construída do zero na semana Omnistack 11.0 da RocketSeat utilizando 100% da *stack* JavaScript com *Node.js, React.js* e *React Native*. A aplicação consiste em ser um sistema para *ONGs* pequenas, onde na parte *web* as *ONGs* cadastram o caso e o valor, e na parte *mobile* há o contato da *ONG* para quem quiser ser o herói do caso.
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -10,9 +11,21 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
